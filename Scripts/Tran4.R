639917ab543a2d8c4285d7cfc206b675e90da73f
VictorTran808 <victoramostran@gmail.com>
2020-02-05 18:20:25 -1000

    commit r markdown file

--- Scripts/Tran4.R
+++ Scripts/Tran4.R
@@ -6,7 +6,7 @@
 #initialize working directory and tidyverse package
 setwd("/Users/victor/Desktop/myproject")
 library(tidyverse)
-
+library(RColorBrewer)
 #Importing in 2010 county data
 mydata<- read_csv("C:/Users/victor/Desktop/Economics_R_Code/Dataset/nhgis0002_ds172_2010_county.csv")
 View(mydata)
@@ -16,24 +16,46 @@ help("select")
 #Cleaning up data selecting specific variables under a specific filter
 cleandata <- select(mydata, "STATE", "H7X005", "H7W002", "H7W005", "H7V001")
 view(cleandata)
+
+is.na(cleandata)
+
 CaliforniaData <-filter(cleandata,  STATE == "California")
 view(CaliforniaData)
 #Creating variable objects from new California datatable
+#' H7X005 is the number of asian in the california county
+#' H7v001 is the population in the california county
+#' H7w002 is the number living in urban area
+#' H7w005 is the number living in rural area
 numberAsian <- CaliforniaData$H7X005/100
 population <- CaliforniaData$H7V001/100
 numberUrban <- CaliforniaData$H7W002/ 100 
 numberRural <- CaliforniaData$H7W005/100
 
-ggplot(CaliforniaData) + geom_point(aes(x = numberAsian, y= numberUrban, color = population)) +
-  xlab("number of asian") + 
-  ylab("asian in urban area per hundred") +
-  ggtitle("Do asian like to live in urban area in California?")
+#Note: remember that these variable were divied by 100 !!!
+mean(population) # 6423.096
+mean(numberAsian) #838.1047 
+summary(CaliforniaData)
 
-ggplot(CaliforniaData) + 
-  geom_point(aes(x = numberRural, y= numberUrban, size = numberAsian, color = numberAsian)) +
+Cali_graph <- ggplot(CaliforniaData) + 
+  geom_point(aes(x = numberRural, y= numberUrban, size = population, color = numberAsian)) +
+ # geom_smooth(method = "auto") +
   xlab("asian in rural area per hundred") + 
   ylab("asian in urban area per hundred") +
-  ggtitle("Asian in Urban vs Rural area in California?")
+  ggtitle("Asian in Urban vs Rural area per California County")
+
+#Cali_graph + scale_fill_manual(values = c("skyblue", "royalblue", "green", "red"))
+# display.brewer.all()
+# Cali_graph + scale_color_gradient(low = "red", high = "blue")
+
+#Cali_graph + scale_color_brewer(palette = "Set1") 
+# Error: Continuous value supplied to discrete scale
+
+Cali_graph + scale_color_gradient2(midpoint =  8782, low = "blue", high = "red", mid = "purple")
+?scale_alpha
+?aes
+
+Cali_graph + scale_color_gradientn(colours = rainbow(5)) 
+
 
 
 
