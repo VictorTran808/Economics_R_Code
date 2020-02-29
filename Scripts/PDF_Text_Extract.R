66812f64f12b0bc521f5524bfe0f2c2d9ef6f30e
VictorTran808 <victoramostran@gmail.com>
2020-02-29 10:26:13 -1000

    Added pdf extractor script

---
+++ Scripts/PDF_Text_Extract.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/victor/Desktop/Economics_R_Code")
+
+library(pdftools)
+library(tidyverse)
+
+?pdf_text
+PV_text <- pdf_text("C:/Users/victor/Desktop/Economics_R_Code/Dataset/Solar_PV_Installation_In_Honolulu_Sep2017.pdf")%>%
+  readr::read_lines()
+
+PV_text
+
+view(PV_text)
+
+table_5 <- PV_text[467:503]
+table_5
